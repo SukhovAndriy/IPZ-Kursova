d5040fed6919e8e465327e98bb46f8f7900611c5
Andriy <sukhov.andriy@lll.kpi.ua>
2025-05-08 20:23:23 +0300

    new structure

--- game_logic.py
+++ game_logic.py
@@ -1,42 +1,31 @@
-from threading import Lock
+symbols = ['X', 'O']
 
-class TicTacToeGame:
-    WIN_COMBOS = [
-        (0,1,2),(3,4,5),(6,7,8),
-        (0,3,6),(1,4,7),(2,5,8),
-        (0,4,8),(2,4,6)
-    ]
-
+class Game:
     def __init__(self):
         self.board = [''] * 9
-        self.current_player = 0
-        self.lock = Lock()
-        self.restart_votes = [False, False]
+        self.current_turn = 0
 
-    def move(self, player_id, index):
-        with self.lock:
-            if self.board[index] or self.current_player != player_id:
-                return False
-            symbol = ['X','O'][player_id]
-            self.board[index] = symbol
-            return True
+    def make_move(self, idx):
+        if 0 <= idx < 9 and self.board[idx] == '':
+            symbol = symbols[self.current_turn]
+            self.board[idx] = symbol
+            return symbol
+        return None
 
     def check_winner(self, symbol):
-        return any(all(self.board[i] == symbol for i in combo) for combo in self.WIN_COMBOS)
+        combos = [
+            (0,1,2),(3,4,5),(6,7,8),
+            (0,3,6),(1,4,7),(2,5,8),
+            (0,4,8),(2,4,6)
+        ]
+        return any(all(self.board[i] == symbol for i in combo) for combo in combos)
 
     def is_full(self):
-        return '' not in self.board
+        return all(cell != '' for cell in self.board)
 
-    def switch_player(self):
-        self.current_player = 1 - self.current_player
+    def next_turn(self):
+        self.current_turn = 1 - self.current_turn
 
     def reset(self):
-        with self.lock:
-            self.board = ['']*9
-            self.current_player = 0
-            self.restart_votes = [False, False]
-
-    def vote_restart(self, player_id):
-        with self.lock:
-            self.restart_votes[player_id] = True
-            return sum(self.restart_votes)
+        self.board = [''] * 9
+        self.current_turn = 0
