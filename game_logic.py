3867d0837c317a5ded5e764b7d9a37f56c1ef4a5
Andriy <sukhov.andriy@lll.kpi.ua>
2025-05-22 19:37:17 +0300

    another server update

--- game_logic.py
+++ game_logic.py
@@ -1,23 +1,26 @@
+# game_logic.py
 symbols = ['X', 'O']
+
+combos = [
+    (0, 1, 2), (3, 4, 5), (6, 7, 8),
+    (0, 3, 6), (1, 4, 7), (2, 5, 8),
+    (0, 4, 8), (2, 4, 6)
+]
 
 class Game:
     def __init__(self):
         self.board = [''] * 9
         self.current_turn = 0
+        self.symbols = symbols.copy()
 
     def make_move(self, idx):
         if 0 <= idx < 9 and self.board[idx] == '':
-            symbol = symbols[self.current_turn]
+            symbol = self.symbols[self.current_turn]
             self.board[idx] = symbol
             return symbol
         return None
 
     def check_winner(self, symbol):
-        combos = [
-            (0,1,2),(3,4,5),(6,7,8),
-            (0,3,6),(1,4,7),(2,5,8),
-            (0,4,8),(2,4,6)
-        ]
         return any(all(self.board[i] == symbol for i in combo) for combo in combos)
 
     def is_full(self):
@@ -29,3 +32,4 @@ class Game:
     def reset(self):
         self.board = [''] * 9
         self.current_turn = 0
+        self.symbols = symbols.copy()
